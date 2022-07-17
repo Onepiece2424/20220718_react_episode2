8e19a2671f80e8c15bada0fb84c19751b22ce158
Onepiece2424 <79039664+Onepiece2424@users.noreply.github.com>
2022-07-18 06:34:33 +0900

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// document.getElementById("app").innerHTML = `
+// <h1>Hello Vanilla!</h1>
+// <div>
+//   We use the same configuration as Parcel to bundle this sandbox, you can find more
+//   info about Parcel
+//   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+// </div>
+// `;
+
+//従来の文字列と変数の結合方法
+const name = "田中";
+
+const age = 24;
+
+const message = "私の名前は" + name + "です。年齢は" + age + "です。";
+console.log(message);
